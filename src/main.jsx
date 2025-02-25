a431cef5641df96964a4f0ba9612089acfbf3384
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-02-25 13:09:10 +0530

    React Router Dom: Routes Created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { lazy, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+const Home = lazy(() => import("./screen/Home"));
+const About = lazy(() => import("./screen/About"));
+const Stores = lazy(() => import("./screen/Stores"));
+const Categories = lazy(() => import("./screen/Categories"));
+const Login = lazy(() => import("./screen/Login"));
+const Register = lazy(() => import("./screen/Register"));
+const Profile = lazy(() => import("./screen/Profile"));
+const Settings = lazy(() => import("./screen/Settings"));
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/stores" element={<Stores />} />
+                    <Route path="/categories" element={<Categories />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/about" element={<About />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
+);
