04222cc765050832d1b52cf0874e6171e0a3858a
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-02-25 13:09:35 +0530

    Layout is under designing Process

---
+++ src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function Layout() {
+    return (
+        <div className="mobile:bg-neutral-800">
+            <Navbar />
+            <div className="bg-neutral-800 text-neutral-200 w-full min-h-dvh p-4 mobile:px-4 tablet:px-8 laptop:px-16">
+                <Outlet />
+            </div>
+        </div>
+    );
+}
+
+export default Layout;
