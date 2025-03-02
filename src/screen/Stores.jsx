1acc12cb6ce4d8afddd7c9e193f396033b665176
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-02 12:37:22 +0530

    Responsive using grid

--- src/screen/Stores.jsx
+++ src/screen/Stores.jsx
@@ -5,7 +5,7 @@ function Stores() {
     const [toggleCode, setToggleCode] = useState(false);
 
     return (
-        <div className="flex flex-row flex-wrap gap-4 pt-4">
+        <div className="grid laptop:grid-cols-3 mobile:grid-cols-1 tablet:grid-cols-2 gap-4">
             <CouponCard tags={["Amazon", "15% Discount"]} code={23423} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis et distinctio numquam, a ab debitis impedit dolor eaque nam fugit, iusto saepe nemo tenetur delectus!"} expiryDate={"20/2/2026"} title={"15% Discount on every purchase above 299rs"}  />
             <CouponCard tags={["Amazon"]} code={23423} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis et distinctio numquam, a ab debitis impedit dolor eaque nam fugit, iusto saepe nemo tenetur delectus!"} expiryDate={"20/2/2026"} title={"15% Discount on every purchase above 299rs"}  />
             <CouponCard tags={["Amazon"]} code={23423} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis et distinctio numquam, a ab debitis impedit dolor eaque nam fugit, iusto saepe nemo tenetur delectus!"} expiryDate={"20/2/2026"} title={"15% Discount on every purchase above 299rs"}  />
