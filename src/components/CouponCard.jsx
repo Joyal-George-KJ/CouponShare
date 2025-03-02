d35ca958222f1605279e14ad4c4845c09b257386
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-02 12:37:05 +0530

    UX Adjustments

--- src/components/CouponCard.jsx
+++ src/components/CouponCard.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 
-function CouponCard({tags, title, description, expiryDate, code}) {
+function CouponCard({ tags, title, description, expiryDate, code }) {
     const [toggleCode, setToggleCode] = useState(false);
+    const activater = () => {
+        setToggleCode(!toggleCode);
+        navigator.clipboard.writeText(code);
+        setTimeout(() => {
+            setToggleCode(false);
+        }, 1000);
+    };
 
     return (
-        <div className="flex flex-col gap-2 p-4 bg-neutral-800 rounded-lg shadow shadow-neutral-200 w-[32%]">
+        <div className="flex flex-col gap-2 p-4 bg-neutral-800 rounded-lg shadow shadow-neutral-200 w-full">
             <div className="flex flex-row flex-wrap gap-2">
-                {tags.map((val, ind) => <span className="p-1 bg-neutral-700 font-medium capitalize px-2 cursor-pointer" key={ind}>{val}</span>)}
+                {tags.map((val, ind) => (
+                    <span
+                        className="p-1 bg-neutral-700 font-medium capitalize px-2 cursor-pointer"
+                        key={ind}
+                    >
+                        {val}
+                    </span>
+                ))}
             </div>
             <h2 className="text-lg capitalize font-bold text-neutral-100 ">
                 {title}
@@ -16,27 +30,18 @@ function CouponCard({tags, title, description, expiryDate, code}) {
                 {description}
             </p>{" "}
             {/*Coupon Description*/}
-            <span className="text-base text-neutral-400 capitalize">
-                valid till: {expiryDate}
-            </span>{" "}
-            {/*Coupon Expiry Date*/}
-            {toggleCode ? (
-                <input
-                    type="text"
-                    name=""
-                    className="border border-neutral-400 bg-neutral-200 text-neutral-950 p-2 rounded font-medium cursor-pointer"
-                    disabled
-                    id=""
-                    value={code} /*Coupon Code*/
-                />
-            ) : (
-                <button
-                    className="bg-neutral-200 text-neutral-900 py-2 rounded font-medium cursor-pointer uppercase"
-                    onClick={() => setToggleCode(true)}
-                >
-                    Get Code
-                </button>
-            )}
+            <button
+                className="bg-neutral-200 text-neutral-900 py-2 rounded font-medium cursor-pointer capitalize"
+                onClick={activater}
+            >
+                Get Deal
+            </button>
+            {
+                toggleCode && (
+                    <div className="text-green-300 text-center bg-transparent py-2 px-4 rounded font-medium cursor-default select-none capitalize border-2 border-green-300 fixed bottom-6 right-6 animate-slide-in">
+                        Code Copied to Clipboard
+                    </div>)
+            }
         </div>
     );
 }
