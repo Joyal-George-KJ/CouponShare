5cc2676f8fb7cbacd7019888ff747bb97b3f4571
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-04 12:32:27 +0530

    Layout Adjusted

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,7 @@ function Layout() {
     return (
         <div className="mobile:bg-neutral-800">
             <Navbar />
-            <div className="bg-neutral-800 text-neutral-200 w-full min-h-dvh p-4 mobile:px-4 tablet:px-8 laptop:px-16">
+            <div className="bg-neutral-800 text-neutral-200 w-full min-h-[92dvh] p-4 mobile:px-4 tablet:px-8 laptop:px-16">
                 <Outlet />
             </div>
         </div>
