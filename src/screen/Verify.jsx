2f736fc97d6558d03c6c56757abfd5e3cede3748
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-09 13:33:11 +0530

    Verification in progress

--- src/screen/Verify.jsx
+++ src/screen/Verify.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import AppwriteConfig from "../constants/AppwriteConf";
 
 function Verify() {
-  return (
-    <div>Verify</div>
-  )
+    useEffect(() => {
+        let result = new URLSearchParams(window.location.search);
+
+        const Auth = new AppwriteConfig(
+            "https://cloud.appwrite.io/v1",
+            "67c6a635003603605fbc"
+        );
+
+        Auth.updateUserVerification({
+            uid: result.get("userId"),
+            code: result.get("secret"),
+        });
+    });
+
+    return <div>Verify</div>;
 }
 
-export default Verify
+export default Verify;
