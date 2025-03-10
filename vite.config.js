defb8e303f222ac936328774f6476d0edac2c6f7
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-10 13:25:02 +0530

    Page Not loding going back to what it was before

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server: {
-        historyApiFallback: true, // Ensures deep links like `/verify?userId=...` work
-    },
 });
