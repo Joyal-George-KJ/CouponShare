4bff7b41b9a1e87804af47d03e94284f4331e263
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-10 13:20:05 +0530

    Vercel supporting routes towards Verify

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    server: {
+        historyApiFallback: true, // Ensures deep links like `/verify?userId=...` work
+    },
 });
