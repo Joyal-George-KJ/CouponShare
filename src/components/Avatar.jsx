e0a237a6ee9b5416008fac11d49a61db8419e90f
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-15 12:40:29 +0530

    Avatar Component Created - Not Tested

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-function Avatar({pic}) {
-    return (
-        <img
-            src={pic}
-            alt="Profile"
-            className="w-24 h-24 rounded-full border-2 border-neutral-500"
-        />
-    );
+function Avatar({pic, className="w-24 h-24 rounded-full border-2 border-neutral-500"}) {
+    const validateURL = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+    
+    if (validateURL.test(pic)){
+        return (
+            <img
+                src={pic}
+                alt="Profile"
+                className={className}
+            />
+        );
+    } else {
+        return (
+            <div className={`${className} aspect-square flex items-center justify-center bg-neutral-700 text-white`}>
+                {pic?.split(' ').map((word) => word.charAt(0)).join('')}
+            </div>
+        );
+    }
 }
 
 export default Avatar;
