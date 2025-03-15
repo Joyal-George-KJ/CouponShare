40a2a7b802a7d3efcb81d9f47c5edf272716b305
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-15 12:43:32 +0530

    Started to impliment Add Coupons button

---
+++ src/components/AddCoupon.jsx
@@ -0,0 +1,12 @@
+import { PlusSquareIcon } from 'lucide-react'
+import React from 'react'
+
+function AddCoupon() {
+  return (
+    <div>
+        <PlusSquareIcon />
+    </div>
+  )
+}
+
+export default AddCoupon
