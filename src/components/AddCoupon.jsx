a6b0d1de1a000a36ebb5ab20b73dbf015b57c37f
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-17 16:03:26 +0530

    Handle Coupons Function and UI Almost Done

--- src/components/AddCoupon.jsx
+++ src/components/AddCoupon.jsx
@@ -21,7 +21,7 @@ function AddCoupon() {
         }
     };
 
-    const handleLogin = (e) => {
+    const handleCoupon = async (e) => {
         e.preventDefault();
         handleTags({ key: "Enter", target: { value: tag } });
         const validateURL =
@@ -65,55 +65,73 @@ function AddCoupon() {
             </button>
             {toggle && (
                 <div className="min-h-[75dvh] flex items-center justify-center bg-neutral-800 text-white fixed top-0 left-0 right-0 bottom-0 z-50">
-                <div className="bg-neutral-800 p-8 rounded-xl shadow shadow-neutral-500 w-full max-w-md">
-                    <h2 className="text-3xl font-bold text-center">Add Coupon</h2>
-    
-                    <form className="mt-6 space-y-4" onSubmit={handleLogin}>
-                        <input
-                            type="text"
-                            placeholder="Title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            className="w-full p-3 bg-neutral-700 rounded-lg text-white outline-none focus:ring-2 focus:ring-blue-500"
-                            required
-                        />
-                        <input
-                            type="text"
-                            placeholder="Discription"
-                            value={discription}
-                            onChange={(e) => setDiscription(e.target.value)}
-                            className="w-full p-3 bg-neutral-700 rounded-lg text-white outline-none focus:ring-2 focus:ring-blue-500"
-                            required
-                        />
-                        <input
-                            type="text"
-                            placeholder="Tags"
-                            ref={inputRef}
-                            value={tag || tags[tags.length - 1]}
-                            onKeyDown={handleTags}
-                            onChange={(e) => setTag(e.target.value)}
-                            className="w-full p-3 bg-neutral-700 rounded-lg text-white outline-none focus:ring-2 focus:ring-blue-500"
-                            required
-                        />
-    
-                        <div className="flex gap-4">
-                          <button
-                              type="button"
-                              className="w-full bg-red-500 py-3 rounded-lg text-lg font-medium hover:bg-blue-600 transition-all cursor-pointer"
-                              onClick={() => setToggle(!toggle)}
-                          >
-                              Close
-                          </button>
-                          <button
-                              type="submit"
-                              className="w-full bg-blue-500 py-3 rounded-lg text-lg font-medium hover:bg-blue-600 transition-all cursor-pointer"
-                          >
-                              Create
-                          </button>
-                        </div>
-                    </form>
+                    <div className="bg-neutral-800 p-8 rounded-xl shadow shadow-neutral-500 w-full max-w-md">
+                        <h2 className="text-3xl font-bold text-center">
+                            Add Coupon
+                        </h2>
+
+                        <form className="mt-6 space-y-4" onSubmit={handleCoupon}>
+                            <input
+                                type="text"
+                                placeholder="Title"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                className="w-full p-3 bg-neutral-700 rounded-lg text-white outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Description"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                                className="w-full p-3 bg-neutral-700 rounded-lg text-white outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                            {tags.length > 0 && <div className="flex flex-wrap gap-2">
+                                {tags.map((val, ind) => (
+                                    <span className="px-2 py-1 rounded bg-neutral-700 " onClick={() => {
+                                        setTags(tags.filter((tag, index) => index !== ind));
+                                    }} key={ind}>{val}</span>
+                                ))}
+                            </div>}
+                            <input
+                                type="text"
+                                placeholder="Tags"
+                                ref={inputRef}
+                                value={tag}
+                                onKeyDown={handleTags}
+                                onChange={(e) => setTag(e.target.value)}
+                                className="w-full p-3 bg-neutral-700 rounded-lg text-white outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="URL/Code"
+                                ref={inputRef}
+                                value={code}
+                                onChange={(e) => setCode(e.target.value)}
+                                className="w-full p-3 bg-neutral-700 rounded-lg text-white outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+
+                            <div className="flex gap-4">
+                                <button
+                                    type="button"
+                                    className="w-full border text-red-500 hover:text-white border-red-500 py-3 rounded-lg text-lg font-medium hover:bg-red-600 transition-all cursor-pointer"
+                                    onClick={() => setToggle(!toggle)}
+                                >
+                                    Close
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="w-full bg-blue-500 py-3 rounded-lg text-lg font-medium hover:bg-blue-600 transition-all cursor-pointer"
+                                >
+                                    Create
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
             )}
         </div>
     );
