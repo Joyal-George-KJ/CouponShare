1b5b13d3e0f72371dcd076b4a34967836b396c9e
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-18 12:55:02 +0530

    Removed Unnecessary things

--- src/screen/Stores.jsx
+++ src/screen/Stores.jsx
@@ -4,7 +4,6 @@ import AddCoupon from "../components/AddCoupon";
 import AppwriteConfig from "../constants/AppwriteConf";
 
 function Stores() {
-    const [toggleCode, setToggleCode] = useState(false);
     const [coupons, setCoupons] = useState([]);
 
     useEffect(() => {
@@ -30,42 +29,6 @@ function Stores() {
                 <AddCoupon />
             </div>
             <div className="grid laptop:grid-cols-3 mobile:grid-cols-1 tablet:grid-cols-2 gap-4">
-                <CouponCard
-                    tags={["Amazon", "15% Discount"]}
-                    code={23423}
-                    description={
-                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis et distinctio numquam, a ab debitis impedit dolor eaque nam fugit, iusto saepe nemo tenetur delectus!"
-                    }
-                    expiryDate={"20/2/2026"}
-                    title={"15% Discount on every purchase above 299rs"}
-                />
-                <CouponCard
-                    tags={["Amazon"]}
-                    code={23423}
-                    description={
-                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis et distinctio numquam, a ab debitis impedit dolor eaque nam fugit, iusto saepe nemo tenetur delectus!"
-                    }
-                    expiryDate={"20/2/2026"}
-                    title={"15% Discount on every purchase above 299rs"}
-                />
-                <CouponCard
-                    tags={["Amazon"]}
-                    code={23423}
-                    description={
-                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis et distinctio numquam, a ab debitis impedit dolor eaque nam fugit, iusto saepe nemo tenetur delectus!"
-                    }
-                    expiryDate={"20/2/2026"}
-                    title={"15% Discount on every purchase above 299rs"}
-                />
-                <CouponCard
-                    tags={["Amazon"]}
-                    code={23423}
-                    description={
-                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis et distinctio numquam, a ab debitis impedit dolor eaque nam fugit, iusto saepe nemo tenetur delectus!"
-                    }
-                    expiryDate={"20/2/2026"}
-                    title={"15% Discount on every purchase above 299rs"}
-                />
                 {
                     coupons.map((val, ind) => <CouponCard key={ind} {...val} />)
                 }
