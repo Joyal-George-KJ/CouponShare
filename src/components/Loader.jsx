4dcddd6793a051d8f58df5ecd8f2103090dc3ef1
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-20 00:44:24 +0530

    Loader UI adjustment

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -16,7 +16,7 @@ function Loader({ text }) {
         return () => clearInterval(interval);
     }, []);
     return (
-        <div className="bg-neutral-800 text-neutral-200 transition ease-out w-full min-h-dvh flex place-items-center justify-center p-4 mobile:px-4 tablet:px-8 laptop:px-16">
+        <div className="bg-neutral-800 text-neutral-200 transition ease-out w-full min-h-full flex place-items-center justify-center p-4 mobile:px-4 tablet:px-8 laptop:px-16">
             <div className="w-14 aspect-square rounded-full border-8 border-white relative animate-pulse"></div>
             <p
                 className="px-4 animate-pulse text-2xl"
