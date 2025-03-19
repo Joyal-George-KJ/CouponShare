47694b8b973698569034560602b25b94a3683aee
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-20 00:45:23 +0530

    UI adjustments

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Home() {
     return (
-        <div className="relative w-full h-full bg-neutral-800 text-white py-20 flex flex-col items-center text-center">
+        <div className="w-full h-full bg-neutral-800 text-white py-20 flex flex-col items-center mx-auto text-center">
 
             <h1 className="mobile:text-2xl tablet:text-4xl laptop:text-6xl font-extrabold tracking-tight">
                 Save More, Spend Less 💰
@@ -14,9 +14,9 @@ function Home() {
             </p>
 
             <div className="mt-6 flex flex-row gap-4">
-                <Link to={"/stores"} className="bg-blue-500 text-white px-6 py-3 mobile:text-base md:text-lg font-medium rounded-lg hover:bg-blue-600 transition-all shadow shadow-white">
+                <NavLink to={"/stores"} className="bg-blue-500 text-white px-6 py-3 mobile:text-base md:text-lg font-medium rounded-lg hover:bg-blue-600 transition-all shadow shadow-white">
                     Browse Coupons
-                </Link>
+                </NavLink>
                 <button className="bg-white text-blue-500 px-6 py-3 mobile:text-base md:text-lg font-medium rounded-lg hover:bg-neutral-200 transition-all shadow shadow-blue-600">
                     Share a Coupon
                 </button>
