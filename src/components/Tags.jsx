00c4eeac626e121965d15083e22fe223208c87fa
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-22 14:39:35 +0530

    Route change incorporated in the tags component

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 function Tags({ name, count }) {
     return (
-        <NavLink to={`/stores/${name}`} className="text-neutral-100">
+        <NavLink to={`/stores/tags/${name}`} className="text-neutral-100">
             <div className="p-4 bg-neutral-700 rounded-lg w-fit flex justify-center items-center gap-2 transition ease-in-out hover:scale-105 cursor-pointer">
                 <h2 className="text-base capitalize font-semibold text-neutral-100">
                     {name}
