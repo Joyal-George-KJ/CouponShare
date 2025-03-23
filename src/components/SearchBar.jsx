b37295df9426b85eedb578d49083cbbf4e9c16fa
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-23 13:32:35 +0530

    According to resizing change Search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -24,36 +24,45 @@ function SearchBar() {
         navigate(`/stores/${searchSelector}/${searchInput}`);
     };
     return (
-        <form className="flex justify-between items-center max-w-md mx-auto mr-4">
+        <form
+            className="flex w-full justify-between items-center max-w-md mr-4"
+            onSubmit={handleSubmit}
+        >
             {/* Search Type Selector */}
-            <label htmlFor="searchSelector" className="sr-only">
-                Search by
-            </label>
-            <select
-                id="searchSelector"
-                name="searchSelector"
-                className="text-neutral-200 border border-neutral-600 bg-neutral-800 p-2 rounded-l-md outline-0"
-            >
-                <option value="all">All</option>
-                <option value="name">Name</option>
-                <option value="tag">Tag</option>
-            </select>
+            {width > 722 && (
+                <>
+                    <label htmlFor="searchSelector" className="sr-only">
+                        Search by
+                    </label>
+                    <select
+                        id="searchSelector"
+                        name="searchSelector"
+                        className="text-neutral-200 border border-neutral-600 bg-neutral-800 p-2 rounded-l-md outline-0"
+                    >
+                        <option value="all">All</option>
+                        <option value="title">Name</option>
+                        <option value="tags">Tag</option>
+                    </select>
 
-            {/* Search Input */}
-            <label htmlFor="searchInput" className="sr-only">
-                Search
-            </label>
-            <input
-                id="searchInput"
-                type="text"
-                placeholder="Search..."
-                className="outline-0 bg-neutral-800 border border-neutral-600 caret-neutral-200 pl-4 py-2 w-full text-neutral-200 placeholder-neutral-400"
-            />
+                    {/* Search Input */}
+                    <label htmlFor="searchInput" className="sr-only">
+                        Search
+                    </label>
+                    <input
+                        id="searchInput"
+                        type="text"
+                        placeholder="Search..."
+                        className="outline-0 bg-neutral-800 border border-neutral-600 caret-neutral-200 pl-4 py-2 w-full text-neutral-200 placeholder-neutral-400"
+                    />
+                </>
+            )}
 
             {/* Search Button */}
             <button
                 type="submit"
-                className="bg-neutral-700 border border-neutral-600 text-neutral-200 p-2 rounded-r-md hover:bg-neutral-600 transition"
+                className={`bg-neutral-700 border border-neutral-600 text-neutral-200 p-2 hover:bg-neutral-600 transition ${
+                    width > 722 ? "rounded-r-md" : "rounded-md"
+                }`}
                 aria-label="Search"
             >
                 <SearchIcon className="text-neutral-200" />
