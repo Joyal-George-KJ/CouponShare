07a3077680a736446ee55c675c4c610e2c11ba39
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-24 13:08:34 +0530

    Calling that function

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -28,6 +28,7 @@ function Layout() {
             let res = await config.getUserProfile();
             if (res) {
                 dispatch(setUser(res));
+                await config.createUserDB({id: res.userId, email: res.email, name: res.name});
                 setLoading(false);
             } else {
                 setTimeout(() => {
