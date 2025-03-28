2fea0ffaaeb10ba1e57bc999b5882c05eda3c485
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-29 01:13:42 +0530

    In-Progress: Avatar Imporving

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 function Avatar({pic, className="w-24 h-24 rounded-full border-2 border-neutral-500"}) {
     const validateURL = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+    pic = ((pic && validateURL.test(pic)) ? pic : pic?.split(' ').map((word) => word.charAt(0)).join(''));
     
     if (validateURL.test(pic)){
         return (
@@ -14,7 +15,7 @@ function Avatar({pic, className="w-24 h-24 rounded-full border-2 border-neutral-
     } else {
         return (
             <div className={`${className} mobile:w-12 mobile:h-12 aspect-square flex items-center justify-center bg-neutral-700 text-white`}>
-                {pic?.split(' ').map((word) => word.charAt(0)).join('')}
+                {pic}
             </div>
         );
     }
