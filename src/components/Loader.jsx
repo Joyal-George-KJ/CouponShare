5df89b2e5f267d83b3764e23e9e1c81a45807bbf
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-29 12:57:35 +0530

    New Interface Loader done!

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 
 function Loader({ text }) {
     const [loadingText, setLoadingText] = useState(!text && "Loading");
+    const [loader, setLoader] = useState(1);
+
+    
+
     useEffect(() => {
         const interval = setInterval(() => {
             setLoadingText((prev) => {
@@ -12,19 +16,30 @@ function Loader({ text }) {
             });
         }, 1000);
 
-        return () => clearInterval(interval);
+        const loadInterval = setInterval(() => {
+            setLoader(prev => {
+                if (prev >= 100) {
+                    return 1;
+                } else {
+                    return prev+5;
+                }
+            });
+        }, 100);
+
+        return () => {
+            clearInterval(interval);
+            clearInterval(loadInterval);
+        };
     }, []);
     return (
-        <div className="bg-neutral-800 text-neutral-200 transition ease-out w-full min-h-full flex place-items-center justify-center p-4 mobile:px-4 tablet:px-8 laptop:px-16">
-            <div className="w-14 aspect-square rounded-full border-8 border-white relative animate-pulse"></div>
-            <p
-                className="px-4 animate-pulse text-2xl"
-                style={{
-                    minWidth: `${"loading...".length * 20}px`,
-                }}
+        <div className="bg-neutral-800 text-neutral-200 transition ease-out w-full min-h-full flex flex-col place-items-center justify-center p-4 mobile:px-4 tablet:px-8 laptop:px-16">
+            <h2
+                to="/"
+                className="text-3xl font-extrabold text-white tracking-wide"
             >
-                {loadingText}
-            </p>
+                Coupon<span className="text-blue-500">Share</span>
+            </h2>
+            <input className="slider" type="range" name="loading" id="loading" min={1} max={100} value={loader} readOnly />
         </div>
     );
 }
