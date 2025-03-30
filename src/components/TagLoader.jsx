e7190630393d575beb421dff90297a3694f48677
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-03-30 11:04:08 +0530

    Tag Loader Created

---
+++ src/components/TagLoader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TagLoader() {
+    return (
+        <div className="w-fit justify-center items-center bg-neutral-700 animate-pulse flex gap-4 shadow-md p-4 shadow-neutral-900 rounded-md">
+            <div className="w-18 h-4 bg-neutral-600 animate-pulse rounded"></div>
+                <div className="w-6 aspect-square bg-neutral-600 animate-pulse rounded-full"></div>
+        </div>
+    );
+}
+
+export default TagLoader;
