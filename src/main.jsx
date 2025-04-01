571a6ba17ce38a2dae2eb1199c084080ac59b883
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-01 13:01:20 +0530

    Implimented Routes for Error Components

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import Layout from "./Layout";
 import { Provider } from "react-redux";
 import store from "./util/store";
 import NotFound from "./components/404";
+import Unauthorized from "./components/401";
+import Forbidden from "./components/403";
+import ServerError from "./components/500";
 const Home = lazy(() => import("./screen/Home"));
 const About = lazy(() => import("./screen/About"));
 const Stores = lazy(() => import("./screen/Stores"));
@@ -17,23 +20,29 @@ const Settings = lazy(() => import("./screen/Settings"));
 
 createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-    <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="/stores" element={<Stores />} />
-                    <Route path="/stores/:key/:value" element={<Stores key={window.location.pathname} />} />
-                    <Route path="/categories" element={<Categories />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/profile" element={<Profile />} />
-                    <Route path="/settings" element={<Settings />} />
-                    <Route path="/about" element={<About />} />
+        <StrictMode>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="/stores" element={<Stores />} />
+                        <Route
+                            path="/stores/:key/:value"
+                            element={<Stores key={window.location.pathname} />}
+                        />
+                        <Route path="/categories" element={<Categories />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="/about" element={<About />} />
+                    </Route>
+                    <Route path="/401" element={<Unauthorized />} />
+                    <Route path="/403" element={<Forbidden />} />
+                    <Route path="/500" element={<ServerError />} />
                     <Route path="*" element={<NotFound />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>
+                </Routes>
+            </BrowserRouter>
+        </StrictMode>
     </Provider>
 );
