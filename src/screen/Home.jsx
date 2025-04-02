b0cd62157a1942f875c2618c72013147f553615c
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-02 12:45:37 +0530

    Share Coupon button in Home Component done!

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Home() {
+    const user = useSelector((state) => state.user.user);
+    const navigate = useNavigate();
+
+    const couponAddHandle = () => {
+        if (user) {
+            navigate('/stores')
+        } else {
+            navigate('/401')
+        }
+    }
     return (
         <div className="w-full h-full bg-neutral-800 text-white py-20 flex flex-col items-center mx-auto text-center">
 
@@ -17,9 +28,9 @@ function Home() {
                 <NavLink to={"/stores"} className="bg-blue-500 text-white px-6 py-3 mobile:text-base md:text-lg font-medium rounded-lg hover:bg-blue-600 transition-all shadow shadow-white">
                     Browse Coupons
                 </NavLink>
-                <NavLink to={"/stores"} className="bg-white text-blue-500 px-6 py-3 mobile:text-base md:text-lg font-medium rounded-lg hover:bg-neutral-200 transition-all shadow shadow-blue-600">
+                <button onClick={couponAddHandle} className="bg-white text-blue-500 px-6 py-3 mobile:text-base md:text-lg font-medium rounded-lg hover:bg-neutral-200 transition-all shadow shadow-blue-600">
                     Share a Coupon
-                </NavLink>
+                </button>
             </div>
         </div>
     );
