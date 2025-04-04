c957e04f99360ea5091bd063f4dec9673529c813
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-04 13:10:56 +0530

    updated reduceres by adding new coupon global state in store

--- src/util/store.js
+++ src/util/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import couponReducer from "./slices/couponSlice";
 
 const store = configureStore({
     reducer: {
         // Reducers
         user: userReducer,
+        coupon: couponReducer,
     }
 })
 
