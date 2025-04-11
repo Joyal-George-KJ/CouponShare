fa5e0fb600acb5aab178ba455948daf7a812dea1
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-11 13:27:25 +0530

    Loader dark mode done

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 
-function Loader({ text }) {
-    const [loadingText, setLoadingText] = useState(!text && "Loading");
+function Loader() {
     const [loader, setLoader] = useState(1);
-
-    
 
     useEffect(() => {
         const loadInterval = setInterval(() => {
@@ -22,14 +19,14 @@ function Loader({ text }) {
         };
     }, []);
     return (
-        <div className="bg-neutral-800 text-neutral-200 transition ease-out w-full min-h-full flex flex-col place-items-center justify-center p-4 mobile:px-4 tablet:px-8 laptop:px-16">
+        <div className="bg-neutral-200 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-200 transition ease-out w-full min-h-full flex flex-col place-items-center justify-center p-4 mobile:px-4 tablet:px-8 laptop:px-16">
             <h2
                 to="/"
-                className="text-3xl font-extrabold text-white tracking-wide"
+                className="text-3xl font-extrabold text-neutral-800 dark:text-white tracking-wide"
             >
                 Coupon<span className="text-blue-500">Share</span>
             </h2>
-            <input className="slider" type="range" name="loading" id="loading" min={1} max={100} value={loader} readOnly />
+            <input className="slider bg-neutral-400" type="range" name="loading" id="loading" min={1} max={100} value={loader} readOnly />
         </div>
     );
 }
