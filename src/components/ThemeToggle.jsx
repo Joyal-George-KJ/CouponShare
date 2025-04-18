ca32e8bcdaa59a16c97e7c7c9e347864f591e7ec
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-18 13:22:22 +0530

    Theme toggle adjusted

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,23 +4,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { setTheme, toggleTheme } from "../util/slices/themeSlice";
 
 const ThemeToggle = () => {
-  const dispatch = useDispatch();
-  const isDarkMode = useSelector((state) => state.theme.isDarkMode);
+    const dispatch = useDispatch();
+    const isDarkMode = useSelector((state) => state.theme.isDarkMode);
 
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme") || "light";
-    dispatch(setTheme(savedTheme));
-  }, [dispatch]);
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme") || "light";
+        dispatch(setTheme(savedTheme));
+    }, [dispatch]);
 
-  return (
-    <button
-      onClick={() => dispatch(toggleTheme())}
-      className="transition-colors duration-300"
-      aria-label="Toggle Theme"
-    >
-      {isDarkMode ? <span className="text-xl">🌙</span> : <span className="text-xl">☀️</span>}
-    </button>
-  );
+    return (
+        <button
+            onClick={() => dispatch(toggleTheme())}
+            className="transition-colors w-fit mobile:w-full dark:bg-neutral-700 bg-neutral-300 p-2 py-1 pb-2 rounded-full duration-300"
+            aria-label="Toggle Theme"
+        >
+            <span className="text-xl">{isDarkMode ? "🌙" : "☀️"}</span>
+        </button>
+    );
 };
 
 export default ThemeToggle;
